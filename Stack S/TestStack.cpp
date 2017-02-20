37719133b757939cd612b5d1615ec307f50658bf
Rikharthu <uberviolence@gmail.com>
2017-02-20 15:34:22 +0200

    labdarbs 1

---
+++ Stack S/TestStack.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include "stack.h"
+
+int  main()
+{
+	Stack s1;
+	s1.reset();
+	s1.clean();
+	s1.print();
+
+	s1.push(100);
+	s1.print();
+	s1.push(200);
+	s1.push(300);
+	s1.print();
+	cout << endl << "pop from s1 " << s1.pop();
+	s1.print();
+	cout << endl << "at the top of s1 " << s1.topOf();
+	cout << endl << "pop from s1 " << s1.pop();
+	s1.print();
+	cout << endl << "pop from s1 " << s1.pop();
+	s1.print();
+
+	getchar();
+}
